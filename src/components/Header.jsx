d2234142a6d339d7fc21b8a10d8cbd5183ac5b9c
venkatajagadish <jagadishreddy445@gmail.com>
2025-04-16 12:10:05 +0530

    Adding teh context for more componnets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,12 +25,7 @@ export default function Header() {
 
   return (
     <>
-      <CartModal
-        ref={modal}
-        cartItems={cartContext.items}
-        title="Your Cart"
-        actions={modalActions}
-      />
+      <CartModal ref={modal} title="Your Cart" actions={modalActions} />
       <header id="main-header">
         <div id="main-title">
           <img src="logo.png" alt="Elegant model" />
